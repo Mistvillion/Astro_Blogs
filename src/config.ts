e71e7cb520df9dcdc548fafcd70ce0247c1fd4ef
silentnight-sparklerain <964476541@qq.com>
2026-02-20 16:44:49 +0800

    修改banner和avatar

--- src/config.ts
+++ src/config.ts
@@ -12,12 +12,12 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Blogs",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 360, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -67,7 +67,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Bilibili",
 			icon: "fa6-brands:bilibili",
-			url: "https://www.bilibili.com",
+			url: "https://space.bilibili.com/1407889438?spm_id_from=333.1007.0.0",
 		},
 		{
 			name: "GitHub",
